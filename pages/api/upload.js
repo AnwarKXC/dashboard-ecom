45064290328815eba311067ae00ac4a1b80cf071
AnwarK.code <115826335+AnwarKXC@users.noreply.github.com>
2023-06-02 12:22:45 +0300

    edit in admins emails

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -11,7 +11,7 @@ const bucketName = 'next-ecommerce-anwar';
 
 export default async function handle ( req, res ) {
    await mongooseConnect();
-   // await isAdminRequest( req, res );
+   await isAdminRequest( req, res );
 
    const form = new multiparty.Form();
    const { fields, files } = await new Promise( ( resolve, reject ) => {
