53fc3368df8ffbb6e965eef82ad5989223c65167
AnwarK.code <115826335+AnwarKXC@users.noreply.github.com>
2023-06-03 05:22:53 +0300

    add

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -14,6 +14,7 @@ import { getServerSession } from 'next-auth';
 export default NextAuth( {
    providers: [
       GoogleProvider( {
+         secret: process.env.SECRET,
          clientId: process.env.GOOGLE_ID,
          clientSecret: process.env.GOOGLE_SECRET,
          authorization: {
